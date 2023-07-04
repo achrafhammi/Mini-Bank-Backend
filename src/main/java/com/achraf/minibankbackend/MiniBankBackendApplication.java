747d6eb3063112213f4b799a2f79d3ab022fbb1c
Malcomer123 <achrafhammi29@gmail.com>
2023-07-04 14:50:37 +0100

    Project Initialized

---
+++ src/main/java/com/achraf/minibankbackend/MiniBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.achraf.minibankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBankBackendApplication.class, args);
+	}
+
+}
